4971ad7686390b11f3833d3a212ac61912fc1dab
ldw1986hf123 <ldw1986hf123@163.com>
2025-03-01 22:45:08 +0800

    common init

---
+++ uc-app/src/main/java/com/ldw/microservice/controller/UCController.java
@@ -0,0 +1,15 @@
+package com.ldw.microservice.controller;
+
+import com.ldw.common.vo.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UCController {
+
+    @GetMapping("/getUserById")
+    public Result getUserById() {
+        return Result.success("steven");
+    }
+}
