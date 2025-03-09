e0723d66cc7e95c1e55db929894e2a19b0456c0b
ldw1986hf123 <ldw1986hf123@163.com>
2025-03-10 05:21:01 +0800

    nacos作为服务发现和治理

--- uc-app/src/main/java/com/ldw/microservice/controller/UCApplication.java
+++ uc-app/src/main/java/com/ldw/microservice/controller/UCApplication.java
@@ -2,11 +2,10 @@ package com.ldw.microservice.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient  // 启用 Nacos 服务发现
 public class UCApplication {
     public static void main(String[] args) {
         SpringApplication.run(UCApplication.class, args);
