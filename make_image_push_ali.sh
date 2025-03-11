f3ea710b13276221d4a2e0b5ff23f20f1ad7f9b3
ldw1986hf123 <ldw1986hf123@163.com>
2025-03-11 08:09:01 +0800

    docker-compose管理打包镜像和部署

--- make_image_push_ali.sh
+++ make_image_push_ali.sh
@@ -3,5 +3,5 @@ docker build -t ldw_microservice/microservice-uc:latest .
 docker push ldw_microservice/microservice-uc:latest
 
 # 拉取新镜像并重启 service-user
-docker-compose pull service-user
-docker-compose up -d service-user
+docker-compose pull microservice
+docker-compose up -d microservice
