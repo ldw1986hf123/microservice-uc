3cc8c4cdea50e5c29cd0499bbc545b06f065b07c
ldw1986hf123 <ldw1986hf123@163.com>
2025-03-16 07:57:42 +0800

    启动类上，有EnableFeignClients注解，自己实现的接口才能被注册到nacos上，被启动服务调用

--- make_image_push_ali.sh
+++
@@ -1,7 +0,0 @@
-mvn clean package -DskipTests
-docker build -t ldw_microservice/microservice-uc:latest .
-docker push ldw_microservice/microservice-uc:latest
-
-# 拉取新镜像并重启 service-user
-docker-compose pull microservice
-docker-compose up -d microservice
--- uc-app/src/main/java/com/ldw/microservice/controller/UCApplication.java
+++ uc-app/src/main/java/com/ldw/microservice/controller/UCApplication.java
@@ -3,9 +3,11 @@ package com.ldw.microservice.controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient  // 启用 Nacos 服务发现
+@EnableFeignClients  // 启用 Feign
 public class UCApplication {
     public static void main(String[] args) {
         SpringApplication.run(UCApplication.class, args);
