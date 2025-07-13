a1fe47ed865a40dba8c83df42fae3af8fbc4829d
ldw1986hf123 <ldw1986hf123@163.com>
2025-07-14 00:55:16 +0800

    remove spring security

--- uc-app/src/main/java/com/ldw/microservice/annotation/JwtAuthenticationFilter.java
+++ uc-app/src/main/java/com/ldw/microservice/annotation/JwtAuthenticationFilter.java
@@ -1,81 +1,81 @@
-package com.ldw.microservice.annotation;
-
-import com.ldw.microservice.config.JwtTokenProvider;
-import io.jsonwebtoken.Claims;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
-import org.springframework.util.AntPathMatcher;
-import org.springframework.util.StringUtils;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-
-/**
- * JWT认证过滤器
- */
-@Component
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
-
-    private static final List<String> WHITELIST = Arrays.asList(
-            "/auth/login", "/auth/register", "/auth/sendSMSCode", "/auth/confirmLogin", "/auth/tartCaptcha", "/auth/tartCaptchaV2",
-            "/swagger-ui/**", "/v3/api-docs/**", "/webjars/**", "/actuator/**"
-    );
-
-    private static final AntPathMatcher pathMatcher = new AntPathMatcher();
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain)
-            throws ServletException, IOException {
-
-        String requestURI = request.getRequestURI();
-
-        // 跳过白名单
-        if (isWhitelisted(requestURI)) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
-        String token = getTokenFromRequest(request);
-
-        if (StringUtils.hasText(token) ) {
-            UserDetails userDetails =null;
-
-            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                    userDetails,
-                    null,
-                    userDetails.getAuthorities()
-            );
-            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
-            // 设置到上下文中
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        }
-
-        filterChain.doFilter(request, response);
-    }
-
-    private boolean isWhitelisted(String uri) {
-        return WHITELIST.stream().anyMatch(pattern -> pathMatcher.match(pattern, uri));
-    }
-
-    private String getTokenFromRequest(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
-        }
-        return null;
-    }
-}
+//package com.ldw.microservice.annotation;
+//
+//import com.ldw.microservice.config.JwtTokenProvider;
+//import io.jsonwebtoken.Claims;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+//import org.springframework.stereotype.Component;
+//import org.springframework.util.AntPathMatcher;
+//import org.springframework.util.StringUtils;
+//import org.springframework.web.filter.OncePerRequestFilter;
+//
+//import javax.servlet.FilterChain;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.util.Arrays;
+//import java.util.List;
+//
+///**
+// * JWT认证过滤器
+// */
+//@Component
+//public class JwtAuthenticationFilter extends OncePerRequestFilter {
+//
+//
+//    private static final List<String> WHITELIST = Arrays.asList(
+//            "/auth/login", "/auth/register", "/auth/sendSMSCode", "/auth/confirmLogin", "/auth/tartCaptcha", "/auth/tartCaptchaV2",
+//            "/swagger-ui/**", "/v3/api-docs/**", "/webjars/**", "/actuator/**"
+//    );
+//
+//    private static final AntPathMatcher pathMatcher = new AntPathMatcher();
+//
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request,
+//                                    HttpServletResponse response,
+//                                    FilterChain filterChain)
+//            throws ServletException, IOException {
+//
+//        String requestURI = request.getRequestURI();
+//
+//        // 跳过白名单
+//        if (isWhitelisted(requestURI)) {
+//            filterChain.doFilter(request, response);
+//            return;
+//        }
+//
+//        String token = getTokenFromRequest(request);
+//
+//        if (StringUtils.hasText(token) ) {
+//            UserDetails userDetails =null;
+//
+//            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+//                    userDetails,
+//                    null,
+//                    userDetails.getAuthorities()
+//            );
+//            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+//
+//            // 设置到上下文中
+//            SecurityContextHolder.getContext().setAuthentication(authentication);
+//        }
+//
+//        filterChain.doFilter(request, response);
+//    }
+//
+//    private boolean isWhitelisted(String uri) {
+//        return WHITELIST.stream().anyMatch(pattern -> pathMatcher.match(pattern, uri));
+//    }
+//
+//    private String getTokenFromRequest(HttpServletRequest request) {
+//        String bearerToken = request.getHeader("Authorization");
+//        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+//            return bearerToken.substring(7);
+//        }
+//        return null;
+//    }
+//}
